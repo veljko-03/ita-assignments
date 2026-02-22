2f63e445a1de6d209741b235235b3ba5264d9296
veljko-03 <vspasic03@gmail.com>
2026-02-22 18:25:09 +0100

    chore: add js variables

--- e-commerce/js/main.js
+++ e-commerce/js/main.js
@@ -1,1 +1,23 @@
-// JS code
+// Definisanje konstanti/promenljivih
+
+const PRODUCT1_NAME = "Knjiga 1";
+const PRODUCT1_PRICE = 899;
+const PRODUCT1_QTY = 2;
+
+const PRODUCT2_NAME = "Knjiga 2";
+const PRODUCT2_PRICE = 999;
+const PRODUCT2_QTY = 3;
+
+const VAT_RATE = 0.2;
+const CURRENCY = "EUR";
+const USD_PER_EUR = 1.16;
+
+const RAW_COUPON = "SAVE10";
+
+// Type output
+console.log(typeof PRODUCT1_NAME);
+console.log(typeof PRODUCT1_PRICE);
+console.log(typeof PRODUCT1_QTY);
+console.log(typeof VAT_RATE);
+console.log(typeof CURRENCY);
+console.log(typeof RAW_COUPON);
