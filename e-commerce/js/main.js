946e5c073301847329e3a0878e06ed1568e03d68
veljko-03 <vspasic03@gmail.com>
2026-02-22 20:01:52 +0100

    feat: dodajNaIznos and total functions

--- e-commerce/js/main.js
+++ e-commerce/js/main.js
@@ -1,15 +1,20 @@
-// Definisanje konstanti/promenljivih
+// Definisanje varijabla
+
+let iznos = 0;
 
 const PRODUCT1_NAME = "Knjiga 1";
-const PRODUCT1_PRICE = 899;
+const PRODUCT1_PRICE = 24;
 const PRODUCT1_QTY = 2;
 
 const PRODUCT2_NAME = "Knjiga 2";
-const PRODUCT2_PRICE = 999;
+const PRODUCT2_PRICE = 20;
 const PRODUCT2_QTY = 3;
 
+const PRODUCT3_PRICE = 14;
+const PRODUCT4_PRICE = 10;
+
 const VAT_RATE = 0.2;
-const CURRENCY = "EUR";
+const CURRENCY = "USD";
 const USD_PER_EUR = 1.16;
 
 const RAW_COUPON = "SAVE10";
@@ -52,4 +57,28 @@ function login() {
   } else {
     console.log(false);
   }
+}
+
+// Dodavanje cene
+function dodajNaIznos(cena) {
+  iznos += cena;
+  console.log("Trenutni iznos:", iznos);
+}
+
+// Demonstracija rada
+/*
+Ovo sam stavio kao komentar jer u suprotnom izračunaće ukupan iznos čim se stranica otvori,
+što znači da svakim klikom na dugme 'Dodaj u korpu', ukupna cena će se računati počevši od 68.
+Ovako, ukupna cena (klikom na dugme) se računa od nule. 
+*/
+/*
+dodajNaIznos(PRODUCT1_PRICE);
+dodajNaIznos(PRODUCT2_PRICE);
+dodajNaIznos(PRODUCT3_PRICE);
+dodajNaIznos(PRODUCT4_PRICE);
+*/
+
+// Ukupan iznos
+function total() {
+  alert("Ukupan iznos je: " + iznos + " " + CURRENCY);
 }
