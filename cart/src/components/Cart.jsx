970963ca66e9ccfc3b1fa844dfb17f4b13914192
veljko-03 <vspasic03@gmail.com>
2026-07-14 19:36:05 +0200

    CartItem component

--- cart/src/components/Cart.jsx
+++ cart/src/components/Cart.jsx
@@ -1,10 +1,53 @@
 import "../styles/Cart.css"
-import CartItem from "./CartItem";
+import CartItem from "./CartItem"
+
+const products = [
+  {
+    id: 1,
+    name: "Blazer",
+    price: 95,
+    color: "Beige",
+    size: "S",
+    quantity: 1,
+    image: "products/blazer.png"
+  },
+  {
+    id: 2,
+    name: "Chunky Knit Sweater",
+    price: 55,
+    color: "Brown",
+    size: "M",
+    quantity: 1,
+    image: "products/sweater.png"
+  },
+  {
+    id: 3,
+    name: "Mesh Sleeve Blouse",
+    price: 32,
+    color: "Yellow",
+    size: "S",
+    quantity: 1,
+    image: "products/blouse.png"
+  },
+  {
+    id: 4,
+    name: "Retro Jeans",
+    price: 70,
+    color: "Black",
+    size: "S",
+    quantity: 1,
+    image: "products/jeans.png"
+  }
+]
 
 const Cart = () => (
   <>
     <h1>Cart</h1>
-    <CartItem />
+
+    {products.map((item) => (
+      <CartItem key={item.id} item={item} />
+    ))}
   </>
-);
-export default Cart;
+)
+
+export default Cart
--- cart/src/components/CartItem.jsx
+++ cart/src/components/CartItem.jsx
@@ -1,5 +1,21 @@
 import "../styles/Cart.css"
 
-const CartItem = () => <h2>Cart Item</h2>
+const CartItem = ({ item }) => (
+  <div className="cart-item">
+    <img
+      src={item.image}
+      alt={item.name}
+      className="cart-item-image"
+    />
+
+    <div className="cart-item-details">
+      <h3>{item.name}</h3>
+      <p>Boja: {item.color}</p>
+      <p>Veličina: {item.size}</p>
+      <p>Cena: {item.price} $</p>
+      <p>Količina: {item.quantity}</p>
+    </div>
+  </div>
+)
 
 export default CartItem
