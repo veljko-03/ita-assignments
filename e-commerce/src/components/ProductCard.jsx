9aac270294ec333e07029c3d255176fa576a5211
veljko-03 <vspasic03@gmail.com>
2026-08-09 18:59:55 +0200

    shop page add to cart

--- e-commerce/src/components/ProductCard.jsx
+++ e-commerce/src/components/ProductCard.jsx
@@ -1,18 +1,33 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
 import "../styles/Shop.css"
 import Button from "../components/Button"
+import { CartContext } from "../store/context/CartContext"
 
-const ProductCard = ({ product }) => (
-  <article className="product-card">
-    <Link to={`/products/${product.id}`} className="product-image-link">
-      <img src="/placeholder.jpg" alt={product.title} className="product-image" />
-    </Link>
+const ProductCard = ({ product }) => {
+  const { addToCart } = useContext(CartContext)
 
-    <h2 className="product-title">{product.title}</h2>
-    <p className="product-price">€{product.price}</p>
+  const handleAddToCart = () => {
+    console.log("Add to cart:", { product })
+    addToCart(product)
+  }
 
-    <Button btnLabel="Add to cart" />
-  </article>
-)
+  return (
+    <article className="product-card">
+      <Link to={`/products/${product.id}`} className="product-image-link">
+        <img
+          src="/placeholder.jpg"
+          alt={product.title}
+          className="product-image"
+        />
+      </Link>
+
+      <h2 className="product-title">{product.title}</h2>
+      <p className="product-price">€{product.price}</p>
+
+      <Button btnLabel="Add to cart" onClick={handleAddToCart} />
+    </article>
+  )
+}
 
 export default ProductCard
